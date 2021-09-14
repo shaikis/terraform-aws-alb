16d198b119f0288197829d5976e2379b0a1c07bd
Ismail Shaik <60819791+shaikis@users.noreply.github.com>
2021-09-14 10:07:42 +0530

    Update outputs.tf
    added additional output variable

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "app_lb_target_group_arn"{
+output "app_lb_target_group_arn" {
     value = aws_lb_target_group.app_lb_target_group.arn
 }
 
 output "app_lb_arn" {
     value = aws_lb.main_app_lb.arn
+}
+
+output "alb_app_http_sg_id" {
+    value = aws_security_group.alb_app_http_sg.id
 }
